ab42f532a771cefcdfe90b0c6da104158a0994ae
Leticia Garcez <lpgarcez@inf.ufpel.edu.br>
2021-12-05 22:12:55 -0300

    adicionados modelos de musicas

---
+++ Codigo-Fonte-Projeto/Musicas/deckTheHalls.rb
@@ -0,0 +1,157 @@
+#DO sib la sol fa sol la fa / Deck the halls with boughts of holly
+set_volume!50
+play :c6
+sleep 0.4
+play :bb5
+sleep 0.5
+play :a5
+sleep 0.4
+play :g5
+sleep 0.3
+play :f5
+sleep 0.3
+play :g5
+sleep 0.3
+play :a5
+sleep 0.3
+play :f5
+sleep 0.5
+
+#sol la sib sol la, sol fa mi fa / Fa la la la la, la la la la
+play :g5
+sleep 0.3
+play :a5
+sleep 0.3
+play :bb5
+sleep 0.3
+play :g5
+sleep 0.3
+play :a5
+sleep 0.5
+
+play :g5
+sleep 0.3
+play :f5
+sleep 0.3
+play :e5
+sleep 0.5
+play :f5
+sleep 0.5
+
+#DO sib la sol fa sol la fa / Tis the season to be jolly
+play :c6
+sleep 0.35
+play :bb5
+sleep 0.45
+play :a5
+sleep 0.35
+play :g5
+sleep 0.25
+play :f5
+sleep 0.3
+play :g5
+sleep 0.3
+play :a5
+sleep 0.3
+play :f5
+sleep 0.5
+
+#sol la sib sol la, sol fa mi fa / Fa la la la la, la la la la
+play :g5
+sleep 0.3
+play :a5
+sleep 0.3
+play :bb5
+sleep 0.3
+play :g5
+sleep 0.3
+play :a5
+sleep 0.5
+
+play :g5
+sleep 0.3
+play :f5
+sleep 0.3
+play :e5
+sleep 0.5
+play :f5
+sleep 0.5
+
+#sol la sib sol la sib DO sol / Don we now our gay apparel
+play :g5
+sleep 0.3
+play :a5
+sleep 0.3
+play :bb5
+sleep 0.4
+play :g5
+sleep 0.3
+play :a5
+sleep 0.4
+play :bb5
+sleep 0.3
+play :C6
+sleep 0.3
+play :g5
+sleep 0.5
+
+#la si DO RE MI FA MI RE DO / Fa la la la la la la la la
+play :a5
+sleep 0.3
+play :b5
+sleep 0.4
+play :c6
+sleep 0.4
+play :d6
+sleep 0.4
+play :e6
+sleep 0.5
+
+play :f6
+sleep 0.3
+play :e6
+sleep 0.3
+play :d6
+sleep 0.4
+play :c6
+sleep 0.5
+#DO sib la sol fa sol la fa / Troll the ancient Yule tide carol
+play :c6
+sleep 0.4
+play :bb5
+sleep 0.5
+play :a5
+sleep 0.4
+play :g5
+sleep 0.3
+play :f5
+sleep 0.3
+play :g5
+sleep 0.3
+play :a5
+sleep 0.3
+play :f5
+sleep 0.5
+
+#RE RE RE RE DO, sib la sol fa/ Fa la la la la, la la la la
+play :d6
+sleep 0.3
+play :d6
+sleep 0.3
+play :d6
+sleep 0.3
+play :d6
+sleep 0.3
+play :c6
+sleep 0.5
+
+play :bb5
+sleep 0.3
+play :a5
+sleep 0.3
+play :g5
+sleep 0.5
+play :f5
+sleep 0.4
+
+
