d4063e9e0b86b3fbe0415fb5718a0d94e31f4e5d
Leticia Pegoraro Garcez <lpgarcez@inf.ufpel.edu.br>
2021-12-26 14:44:02 -0300

    adicionado controle dos LEDs no código fonte

--- Codigo-Fonte-Projeto/tocaMusicaNatal/tocaMusicaNatal.ino
+++ Codigo-Fonte-Projeto/tocaMusicaNatal/tocaMusicaNatal.ino
@@ -1,96 +1,180 @@
-#define buzzer A5
-
-#define NOTE_C5  523
-#define NOTE_CS5 554
-#define NOTE_D5  587
-#define NOTE_DS5 622
-#define NOTE_E5  659
-#define NOTE_F5  698
-#define NOTE_FS5 740
-#define NOTE_G5  784
-#define NOTE_GS5 831
-#define NOTE_A5  880
-#define NOTE_AS5 932
-#define NOTE_B5  988
-#define NOTE_C6  1047
-#define NOTE_CS6 1109
-#define NOTE_D6  1175
-#define NOTE_DS6 1245
-#define NOTE_E6  1319
-#define NOTE_F6  1397
-#define NOTE_FS6 1480
-#define NOTE_G6  1568
-#define NOTE_GS6 1661
-#define NOTE_A6  1760
-#define NOTE_AS6 1865
-#define NOTE_B6  1976
+//Notas extraidas da biblioteca pitches
+#define NOTE_C5  1047
+#define NOTE_D5 1175
+#define NOTE_E5  1319
+#define NOTE_F5  1397
+#define NOTE_G5  1568
+#define NOTE_A5  1760
+#define NOTE_AS5 1865
+#define NOTE_B5 1976
+#define NOTE_C6  2093
+#define NOTE_D6  2349
+#define NOTE_E6  2637
+#define NOTE_F6  2794
 
 
-#define sb 13//soundBuzzer
-#define btn1 7//button
-#define btn2 8//button
+//Pinos
+#define buzzer 5
+#define btn1 A0
+#define btn2 A1
+#define btn3 A2
+
+#define verde1 13
+#define amarelo1 12
+#define azul1 11
+#define vermelho1 10
+#define verde2 9
+#define amarelo2 8
+#define azul2 7
+#define vermelho2 6
+
 
 //funções
-void play_soft_kitty(void);
-void play_do_re_mi(void);
-void apaga_led(void);
-void tocaMusica(int * vetorNotas, int * vetorDelays,int qtdeNotas);
-
-void playSilentNight();
-void playDeckTheHalls();
-void playJingleBell();
+void tocaMusica(int * vetorNotas, int * vetorDelays, int qtdeNotas);
+void tocaSilentNight();
+void tocaDeckTheHalls();
+void tocaJingleBell();
+void acendeLEDs(bool acendeVerde1, bool acendeAmarelo1, bool acendeAzul1, bool acendeVermelho1, bool acendeVerde2, bool acendeAmarelo2, bool acendeAzul2, bool acendeVermelho2);
+void acendeNota(int nota);
 
 void setup() {
-  
-  pinMode(sb, OUTPUT);
+
   pinMode(btn1, INPUT);
   pinMode(btn2, INPUT);
+  pinMode(btn3, INPUT);
+
+  pinMode(verde1, OUTPUT);
+  pinMode(amarelo1, OUTPUT);
+  pinMode(azul1, OUTPUT);
+  pinMode(vermelho1, OUTPUT);
+
+  pinMode(verde2, OUTPUT);
+  pinMode(amarelo2, OUTPUT);
+  pinMode(azul2, OUTPUT);
+  pinMode(vermelho2, OUTPUT);
+
+  pinMode(buzzer, OUTPUT);
+
+
+       acendeLEDs(0,0,0,0,0,0,0,0);
+
 }
 
 void loop() {
- 
-    playDeckTheHalls();
 
-    noTone(sb);//garante que o buzzer não vai receber nenhum lixo
-  delay(3000);
-  
-  playJingleBell();
-
-    noTone(sb);//garante que o buzzer não vai receber nenhum lixo
-  delay(3000);
-  
-}
-
-
-void playJingleBell(){
-  //Notas B5,D6,G5,A5,C6,D5,E5,AS5
-  int notas[] = {NOTE_B5,NOTE_B5,NOTE_B5,NOTE_B5,NOTE_B5,NOTE_B5,NOTE_B5,NOTE_D6,NOTE_G5,NOTE_A5,NOTE_B5,NOTE_C6,NOTE_C6,NOTE_C6,NOTE_B5,NOTE_B5,NOTE_B5,NOTE_A5,NOTE_B5,NOTE_A5,NOTE_B5,NOTE_A5,NOTE_D6,NOTE_B5,NOTE_B5,NOTE_B5,NOTE_B5,NOTE_B5,NOTE_B5,NOTE_B5,NOTE_D6,NOTE_G5,NOTE_A5,NOTE_B5,NOTE_C6,NOTE_C6,NOTE_C6,NOTE_B5,NOTE_B5,NOTE_B5,NOTE_B5,NOTE_D6,NOTE_D6,NOTE_C6,NOTE_A5,NOTE_G5,NOTE_D5,NOTE_B5,NOTE_A5,NOTE_G5,NOTE_D5,NOTE_D5,NOTE_D5,NOTE_B5,NOTE_A5,NOTE_G5,NOTE_E5,NOTE_E5,NOTE_E5,NOTE_C6,NOTE_B5,NOTE_A5,NOTE_D6,NOTE_D6,NOTE_D6,NOTE_E6,NOTE_D6,NOTE_A5,NOTE_C6,NOTE_B5,NOTE_D5,NOTE_B5,NOTE_A5,NOTE_G5,NOTE_D5,NOTE_D5,NOTE_D5,NOTE_B5,NOTE_A5,NOTE_G5,NOTE_E5,NOTE_E5,NOTE_E5,NOTE_C6,NOTE_B5,NOTE_A5,NOTE_D6,NOTE_D6,NOTE_D6,NOTE_E6,NOTE_D6,NOTE_A5,NOTE_AS5,NOTE_B5};
-  int delays[] = {300,300,500,300,300,600,400,300,200,300,600,300,300,400,300,300,600,400,300,300,200,400,600,300,300,500,300,300,600,400,300,200,300,600,300,300,400,300,300,300,400,400,300,300,200,600,300,300,400,300,500,700,300,300,400,300,500,700,300,300,400,300,400,400,300,300,300,400,300,800,300,300,400,300,500,700,300,300,400,300,500,700,300,300,400,300,400,400,300,300,300,400,300,800};
-  tocaMusica(notas, delays, 94 );
-  
-}
-
-void playDeckTheHalls(){
-  //Notas C6,AS5,A5,G5,F5,B5,D6,E6,F6
-  int notas[] = {NOTE_C6,NOTE_AS5,NOTE_A5,NOTE_G5,NOTE_F5,NOTE_G5,NOTE_A5,NOTE_F5,NOTE_G5,NOTE_A5,NOTE_AS5,NOTE_G5,NOTE_A5,NOTE_G5,NOTE_F5,NOTE_E5,NOTE_F5,NOTE_C6,NOTE_AS5,NOTE_A5,NOTE_G5,NOTE_F5,NOTE_G5,NOTE_A5,NOTE_F5,NOTE_G5,NOTE_A5,NOTE_AS5,NOTE_G5,NOTE_A5,NOTE_G5,NOTE_F5,NOTE_E5,NOTE_F5,NOTE_G5,NOTE_A5,NOTE_AS5,NOTE_G5,NOTE_A5,NOTE_AS5,NOTE_C6,NOTE_G5,NOTE_A5,NOTE_B5,NOTE_C6,NOTE_D6,NOTE_E6,NOTE_F6,NOTE_E6,NOTE_D6,NOTE_C6,NOTE_C6,NOTE_AS5,NOTE_A5,NOTE_G5,NOTE_F5,NOTE_G5,NOTE_A5,NOTE_F5,NOTE_D6,NOTE_D6,NOTE_D6,NOTE_D6,NOTE_C6,NOTE_AS5,NOTE_A5,NOTE_G5,NOTE_F5};
-  int delays[] = {400,500,400,300,300,300,300,500,300,300,300,300,500,300,300,500,500,350,450,350,250,300,300,300,500,300,300,300,300,500,300,300,500,500,300,300,400,300,400,300,300,500,300,400,400,400,500,300,300,400,500,400,500,400,300,300,300,300,500,300,300,300,300,500,300,300,500,400};
-  tocaMusica(notas, delays,68 );
-  
-}
-
-void playSilentNight(){
-  //Notas G5,A5,E5,D6,B5,C6,F6,E6,C5,F5,D5
-  int notas[] = {NOTE_G5,NOTE_A5,NOTE_G5,NOTE_E5,NOTE_G5,NOTE_A5,NOTE_G5,NOTE_E5,NOTE_D6,NOTE_D6,NOTE_B5,NOTE_C6,NOTE_C6,NOTE_G5,NOTE_A5,NOTE_A5,NOTE_C6,NOTE_B5,NOTE_A5,NOTE_G5,NOTE_A5,NOTE_G5,NOTE_E5,NOTE_A5,NOTE_A5,NOTE_C6,NOTE_B5,NOTE_A5,NOTE_G5,NOTE_A5,NOTE_G5,NOTE_E5,NOTE_D6,NOTE_D6,NOTE_F6,NOTE_D6,NOTE_B5,NOTE_C6,NOTE_E6,NOTE_C5,NOTE_G5,NOTE_E5,NOTE_G5,NOTE_F5,NOTE_D5,NOTE_C5};
-  int delays[] = {800,400,400,1300,800,400,400,1300,800,600,1300,800,600,1300,800,600,800,500,500,800,500,500,800,800,600,800,500,500,800,500,500,100,600,600,800,600,500,100,100,600,600,800,600,500,800,1000};
-  tocaMusica(notas, delays,46 );
-  
-}
-
-
-void tocaMusica(int * vetorNotas, int * vetorDelays,int qtdeNotas){
-  for (int c = 0; c< qtdeNotas; c++){
-    vetorNotas[c] == 0 ? noTone(sb) :  tone (sb, vetorNotas[c], vetorDelays[c]);
-    delay(vetorDelays[c]);
-    
+  if (digitalRead(btn1)) {
+    tocaJingleBell();
+  } else if (digitalRead(btn2)) {
+    tocaDeckTheHalls();
+  } else if ( digitalRead(btn3)) {
+    tocaSilentNight();
   }
+  delay(200);
+
+}
+
+void tocaJingleBell() {
+  //Notas B5,D6,G5,A5,C6,D5,E5,AS5
+  int notas[] = {NOTE_B5, NOTE_B5, NOTE_B5, NOTE_B5, NOTE_B5, NOTE_B5, NOTE_B5, NOTE_D6, NOTE_G5, NOTE_A5, NOTE_B5, NOTE_C6, NOTE_C6, NOTE_C6, NOTE_B5, NOTE_B5, NOTE_B5, NOTE_A5, NOTE_B5, NOTE_A5, NOTE_B5, NOTE_A5, NOTE_D6, NOTE_B5, NOTE_B5, NOTE_B5, NOTE_B5, NOTE_B5, NOTE_B5, NOTE_B5, NOTE_D6, NOTE_G5, NOTE_A5, NOTE_B5, NOTE_C6, NOTE_C6, NOTE_C6, NOTE_B5, NOTE_B5, NOTE_B5, NOTE_B5, NOTE_D6, NOTE_D6, NOTE_C6, NOTE_A5, NOTE_G5, NOTE_D5, NOTE_B5, NOTE_A5, NOTE_G5, NOTE_D5, NOTE_D5, NOTE_D5, NOTE_B5, NOTE_A5, NOTE_G5, NOTE_E5, NOTE_E5, NOTE_E5, NOTE_C6, NOTE_B5, NOTE_A5, NOTE_D6, NOTE_D6, NOTE_D6, NOTE_E6, NOTE_D6, NOTE_A5, NOTE_C6, NOTE_B5, NOTE_D5, NOTE_B5, NOTE_A5, NOTE_G5, NOTE_D5, NOTE_D5, NOTE_D5, NOTE_B5, NOTE_A5, NOTE_G5, NOTE_E5, NOTE_E5, NOTE_E5, NOTE_C6, NOTE_B5, NOTE_A5, NOTE_D6, NOTE_D6, NOTE_D6, NOTE_E6, NOTE_D6, NOTE_A5, NOTE_AS5, NOTE_B5};
+  int delays[] = {300, 300, 500, 300, 300, 600, 400, 300, 200, 300, 600, 300, 300, 400, 300, 300, 600, 400, 300, 300, 200, 400, 600, 300, 300, 500, 300, 300, 600, 400, 300, 200, 300, 600, 300, 300, 400, 300, 300, 300, 400, 400, 300, 300, 200, 600, 300, 300, 400, 300, 500, 700, 300, 300, 400, 300, 500, 700, 300, 300, 400, 300, 400, 400, 300, 300, 300, 400, 300, 800, 300, 300, 400, 300, 500, 700, 300, 300, 400, 300, 500, 700, 300, 300, 400, 300, 400, 400, 300, 300, 300, 400, 300, 800};
+  tocaMusica(notas, delays, 94);
+
+}
+
+void tocaDeckTheHalls() {
+  //Notas C6,AS5,A5,G5,F5,B5,D6,E6,F6
+  int notas[] = {NOTE_C6, NOTE_AS5, NOTE_A5, NOTE_G5, NOTE_F5, NOTE_G5, NOTE_A5, NOTE_F5, NOTE_G5, NOTE_A5, NOTE_AS5, NOTE_G5, NOTE_A5, NOTE_G5, NOTE_F5, NOTE_E5, NOTE_F5, NOTE_C6, NOTE_AS5, NOTE_A5, NOTE_G5, NOTE_F5, NOTE_G5, NOTE_A5, NOTE_F5, NOTE_G5, NOTE_A5, NOTE_AS5, NOTE_G5, NOTE_A5, NOTE_G5, NOTE_F5, NOTE_E5, NOTE_F5, NOTE_G5, NOTE_A5, NOTE_AS5, NOTE_G5, NOTE_A5, NOTE_AS5, NOTE_C6, NOTE_G5, NOTE_A5, NOTE_B5, NOTE_C6, NOTE_D6, NOTE_E6, NOTE_F6, NOTE_E6, NOTE_D6, NOTE_C6, NOTE_C6, NOTE_AS5, NOTE_A5, NOTE_G5, NOTE_F5, NOTE_G5, NOTE_A5, NOTE_F5, NOTE_D6, NOTE_D6, NOTE_D6, NOTE_D6, NOTE_C6, NOTE_AS5, NOTE_A5, NOTE_G5, NOTE_F5};
+  int delays[] = {400, 500, 400, 300, 300, 300, 300, 500, 300, 300, 300, 300, 500, 300, 300, 500, 500, 350, 450, 350, 250, 300, 300, 300, 500, 300, 300, 300, 300, 500, 300, 300, 500, 500, 300, 300, 400, 300, 400, 300, 300, 500, 300, 400, 400, 400, 500, 300, 300, 400, 500, 400, 500, 400, 300, 300, 300, 300, 500, 300, 300, 300, 300, 500, 300, 300, 500, 400};
+  tocaMusica(notas, delays, 68);
+
+}
+
+void tocaSilentNight() {
+  //Notas G5,A5,E5,D6,B5,C6,F6,E6,C5,F5,D5
+  int notas[] = {NOTE_G5, NOTE_A5, NOTE_G5, NOTE_E5, NOTE_G5, NOTE_A5, NOTE_G5, NOTE_E5, NOTE_D6, NOTE_D6, NOTE_B5, NOTE_C6, NOTE_C6, NOTE_G5, NOTE_A5, NOTE_A5, NOTE_C6, NOTE_B5, NOTE_A5, NOTE_G5, NOTE_A5, NOTE_G5, NOTE_E5, NOTE_A5, NOTE_A5, NOTE_C6, NOTE_B5, NOTE_A5, NOTE_G5, NOTE_A5, NOTE_G5, NOTE_E5, NOTE_D6, NOTE_D6, NOTE_F6, NOTE_D6, NOTE_B5, NOTE_C6, NOTE_E6, NOTE_C5, NOTE_G5, NOTE_E5, NOTE_G5, NOTE_F5, NOTE_D5, NOTE_C5};
+  int delays[] = {800, 400, 400, 1300, 800, 400, 400, 1300, 800, 600, 1300, 800, 600, 1300, 800, 600, 800, 500, 500, 800, 500, 500, 800, 800, 600, 800, 500, 500, 800, 500, 500, 100, 600, 600, 800, 600, 500, 100, 100, 600, 600, 800, 600, 500, 800, 1000};
+  tocaMusica(notas, delays, 46 );
+
+}
+
+
+void tocaMusica(int * vetorNotas, int * vetorDelays, int qtdeNotas) {
+  for (int c = 0; c < qtdeNotas; c++) {
+    vetorNotas[c] == 0 ? noTone(buzzer) :  tone (buzzer, vetorNotas[c], vetorDelays[c]);
+    acendeNota(vetorNotas[c]);
+    delay(vetorDelays[c]);
+  }
+         acendeLEDs(0,0,0,0,0,0,0,0);
+
+}
+
+void acendeNota(int nota) {
+       acendeLEDs(0,0,0,0,0,0,0,0);
+
+  switch (nota) {
+    case NOTE_C5 :
+acendeLEDs(1,0,0,1,1,0,0,1);
+      break;
+      
+    case NOTE_D5 :
+acendeLEDs(0,1,0,1,0,1,0,1);
+      break;
+      
+    case NOTE_E5 :
+acendeLEDs(1,1,1,1,0,0,0,0);
+      break;
+      
+    case NOTE_F5 :
+acendeLEDs(0,0,0,0,1,1,1,1);
+      break;
+      
+    case NOTE_G5 :
+acendeLEDs(0,1,1,0,0,1,1,0);
+      break;
+      
+    case NOTE_A5 :
+acendeLEDs(0,1,0,1,1,0,1,0);
+      break;
+      
+    case NOTE_AS5 :
+acendeLEDs(0,0,1,1,0,0,1,1);
+      break;
+
+    case NOTE_B5 :
+acendeLEDs(1,1,0,0,1,1,0,0);
+      break;
+
+    case NOTE_C6 :
+acendeLEDs(1,0,1,0,1,0,1,0);
+      break;
+
+    case NOTE_D6 :
+acendeLEDs(1,1,0,0,0,0,1,1);
+      break;
+      
+    case NOTE_E6 :
+acendeLEDs(0,1,1,0,0,0,1,1);
+      break;
+      
+    case NOTE_F6 :
+      acendeLEDs(1,0,0,1,1,1,0,0);
+      break;
+
+    default:
+     acendeLEDs(0,0,0,0,0,0,0,0);
+  }
+}
+
+void acendeLEDs(bool acendeVerde1, bool acendeAmarelo1, bool acendeAzul1, bool acendeVermelho1, bool acendeVerde2, bool acendeAmarelo2, bool acendeAzul2, bool acendeVermelho2){
+
+  digitalWrite(verde1, acendeVerde1);
+  digitalWrite(amarelo1, acendeAmarelo1);
+  digitalWrite(azul1, acendeAzul1);
+  digitalWrite(vermelho1, acendeVermelho1);
+
+  digitalWrite(verde2, acendeVerde2);
+  digitalWrite(amarelo2, acendeAmarelo2);
+  digitalWrite(azul2, acendeAzul2);
+  digitalWrite(vermelho2, acendeVermelho2);
+
 }
